03c36379b75fa1e43c51259ea6b010fd1b859fa6
Kieran Pilkington <kieran776@gmail.com>
2011-11-04 23:12:12 +1300

    Better ruby rendering code for all versions

--- ruby/v3/play-v3.rb
+++ ruby/v3/play-v3.rb
@@ -2,35 +2,34 @@ $:.push(File.dirname(__FILE__))
 
 require 'game-v3'
 
-world = World.new
-
-# Randomized
-150.times do |x|
-  40.times do |y|
-    world.add_cell(x: x, y: y, dead: (rand > 0.2))
+def setup_world
+  @world = World.new
+  150.times do |x|
+    40.times do |y|
+      @world.add_cell(x: x, y: y, dead: (rand > 0.2))
+    end
   end
 end
 
-# Blinker - http://en.wikipedia.org/wiki/File:Game_of_life_blinker.gif
-# Cell.new(world: world, x: 0, y: 1)
-# Cell.new(world: world, x: 1, y: 1)
-# Cell.new(world: world, x: 2, y: 1)
-# Cell.new(world: world, x: 1, y: 0, dead: true)
-# Cell.new(world: world, x: 1, y: 2, dead: true)
-
-while true
-  output = ((world.boundaries[:y][:min])..(world.boundaries[:y][:max])).collect { |y|
-    ((world.boundaries[:x][:min])..(world.boundaries[:x][:max])).collect { |x|
-      cell = world.cell_at(x: x, y: y)
+def render_world
+  ((@world.boundaries[:y][:min])..(@world.boundaries[:y][:max])).collect { |y|
+    ((@world.boundaries[:x][:min])..(@world.boundaries[:x][:max])).collect { |x|
+      cell = @world.cell_at(x: x, y: y)
       (cell ? cell.to_char : ' ')
     }.join
   }.join("\n")
+end
 
+setup_world
+render_world
+
+while true
+  start = Time.now
+  @world.tick!
+  finish = Time.now
+
+  output = render_world
   system('clear')
+  puts "##{@world.tick} - World tick took #{finish - start}"
   puts output
-
-  b = Time.now
-  world.tick!
-  a = Time.now
-  puts "##{world.tick} - World tick took #{a - b}"
 end
