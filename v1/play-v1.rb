e18432d8818967d8f2c5962c2e659531143907c4
Kieran Pilkington <kieran776@gmail.com>
2011-11-04 20:22:21 +1300

    Version 1

---
+++ v1/play-v1.rb
@@ -0,0 +1,33 @@
+$:.push(File.dirname(__FILE__))
+
+require 'game-v1'
+
+world = World.new
+
+# Randomized
+50.times do |x|
+  25.times do |y|
+    Cell.new(world: world, x: x, y: y, dead: (rand > 0.2))
+  end
+end
+
+# Blinker - http://en.wikipedia.org/wiki/File:Game_of_life_blinker.gif
+# Cell.new(world: world, x: 0, y: 1)
+# Cell.new(world: world, x: 1, y: 1)
+# Cell.new(world: world, x: 2, y: 1)
+# Cell.new(world: world, x: 1, y: 0, dead: true)
+# Cell.new(world: world, x: 1, y: 2, dead: true)
+
+while true
+  ((world.boundaries[:y][:min])..(world.boundaries[:y][:max])).each do |y|
+    ((world.boundaries[:x][:min])..(world.boundaries[:x][:max])).each do |x|
+      cell = world.cell_at(x: x, y: y)
+      print (cell ? cell.to_char : ' ')
+    end
+    puts
+  end
+  b = Time.now
+  world.tick!
+  a = Time.now
+  puts "##{world.tick} - World tick took #{a - b}"
+end
