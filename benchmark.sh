3efba58b13f65049e32821f29d10fb6e9c59c1f5
Kieran Pilkington <kieran776@gmail.com>
2025-11-12 16:16:16 +1300

    Ability to benchmark comma separated list

--- benchmark.sh
+++ benchmark.sh
@@ -3,20 +3,46 @@
 SKIP=${SKIP:-0}
 counter=0
 
-for dir in */; do
-  ((counter++))
+if [ -n "$1" ]; then
+  FOLDERS="$1"
+fi
 
-  if [ $counter -le $SKIP ]; then
-    continue
-  fi
-
-  if [ -d "$dir" ]; then
-    benchmark_script="$dir/benchmark.sh"
-    if [ -f "$benchmark_script" ]; then
-      cd "$dir"
-      ./benchmark.sh
-      echo ""
-      cd ".."
+if [ -n "$FOLDERS" ]; then
+  IFS=',' read -ra DIRS <<< "$FOLDERS"
+  for dir in "${DIRS[@]}"; do
+    ((counter++))
+    if [ $counter -le $SKIP ]; then
+      continue
     fi
-  fi
-done
+
+    if [ -d "$dir" ]; then
+      benchmark_script="$dir/benchmark.sh"
+      if [ -f "$benchmark_script" ]; then
+        cd "$dir"
+        ./benchmark.sh
+        echo ""
+        cd - >/dev/null
+      fi
+    else
+      echo "Skipping '$dir' (not a directory)" >&2
+    fi
+  done
+else
+  for dir in */; do
+    ((counter++))
+
+    if [ $counter -le $SKIP ]; then
+      continue
+    fi
+
+    if [ -d "$dir" ]; then
+      benchmark_script="$dir/benchmark.sh"
+      if [ -f "$benchmark_script" ]; then
+        cd "$dir"
+        ./benchmark.sh
+        echo ""
+        cd - >/dev/null
+      fi
+    fi
+  done
+fi
